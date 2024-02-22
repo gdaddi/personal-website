8b128917977132ab9a783bd5ff149ba6cf9ea2a8
gugiek <guglielmo.daddi.96@gmail.com>
2024-02-21 20:24:54 -0800

    Finished the projects page

--- src/components/ProjectsContent.jsx
+++ src/components/ProjectsContent.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './ProjectsContent.css';
-import imageTest from '../assets/hammer.svg';
-import imageEELS from '../assets/EELS.svg';
+import iconMPPD from '../assets/mppd.svg';
+import iconEELS from '../assets/EELS.svg';
+import iconSROC from '../assets/SROC.svg';
 
 const ProjectsContent = () => {
   const [isVisible, setIsVisible] = useState({ eels: false, sroc: false, mppd: false});
@@ -13,6 +14,8 @@ const ProjectsContent = () => {
     }));
   };
 
+// I am not using a project data class here because I want to be able to customize how the content of each project is displayed 
+
   return (
     <div>
       <div className="project-box" onClick={() => toggleVisibility('eels')}>
@@ -23,7 +26,7 @@ const ProjectsContent = () => {
               The <a href='https://www-robotics.jpl.nasa.gov/how-we-do-it/systems/exobiology-extant-life-surveyor-eels/'>Exobiology Extant Life Surveyor (EELS)</a> is a snake-like robotic system
               developed at NASA/JPL. Its purpose is to explore the icy moon of Saturn, Enceladus.
             </p>
-            <img src={imageEELS} alt="EELS Project Icon" />
+            <img src={iconEELS} alt="EELS Project Icon" />
             <p>
               I was involved in the autonomy team, working on risk-aware planning, system-level autonomy, software infrastructure and reinforcement learning.
                Over the project, I took part in three field tests in California and Canada, where I tested the algorithms and software infrastructure that was developed.
@@ -35,10 +38,10 @@ const ProjectsContent = () => {
         <h2>SROC</h2>
         {isVisible.sroc && (
           <div className="project-section">
-            <p>The <a href='https://www.esa.int/ESA_Multimedia/Images/2020/04/SROC'>Space Rider Observer Cube</a> 
-            is a European Space Agency funded project building a small satellite to observe and dock with the Space Rider vehicle. 
+            <p>The <a href='https://www.esa.int/ESA_Multimedia/Images/2020/04/SROC'>Space Rider Observer Cube</a> is a 
+            European Space Agency (ESA) funded   project building a small satellite to observe and dock with the Space Rider vehicle. 
             </p>
-            <img src={imageTest} alt="SROC Project Icon" />
+            <img src={iconSROC} alt="SROC Project Icon" />
             <p>I was involved with developing a graphical simulation environment to validate SROC's guidance and navigation algorithms 
               and supported payload performance analysis.
             </p>
@@ -54,7 +57,7 @@ const ProjectsContent = () => {
               Plasma drilling consists of fracturing ice through high voltage discharges.
               These fractures reduce the ice's thermal conductivity, improving the efficiency of melt probes which rely on thermal energy to penetrate the ice. 
             </p>
-            <img src={imageTest} alt="MPPD project icon" />
+            <img src={iconMPPD} alt="MPPD project icon" />
             <p>
               I was involved with system performance modeling, hardware prototyping, testbed design and experimental validation.
             </p>

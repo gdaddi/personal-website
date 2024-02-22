047e2c7aae9e6985350ff4571368f4c873858156
gugiek <guglielmo.daddi.96@gmail.com>
2024-02-22 05:53:15 -0800

    Added citations from scholar

--- src/data.js
+++ src/data.js
@@ -96,25 +96,86 @@ export const NEWS_ITEMS = [
 
 export const PUBLICATIONS = [
     {
-      id: 'sroc2022',
-      title: 'Space Rider Observer Cube--SROC: a CubeSat mission for proximity operations demonstration',
-      bibtex: `@inproceedings{corpino2022space,
-        title={Space Rider Observer Cube--SROC: a CubeSat mission for proximity operations demonstration},
-        author={Corpino, S and Ammirante, G and Daddi, G and Stesina, F and Corradino, F and Basler, A and Francesconi, A and Branz, F and Van den Eynde, J and others},
-        booktitle={Proc. 73rd International Astronautical Congress (IAC)},
-        year={2022}
-      }`
-    },
-    {
-        id: 'thakker2023eels',
+        id: `thakker2023eels`,
         title: `EELS: Towards Autonomous Mobility in Extreme Terrain with a Versatile Snake Robot with Resilience to Exteroception Failures`,
         bibtex: `@inproceedings{thakker2023eels,
-            title={EELS: Towards Autonomous Mobility in Extreme Terrain with a Versatile Snake Robot with Resilience to Exteroception Failures},
-            author={Thakker, Rohan and Paton, Michael and Strub, Marlin P and Swan, Michael and Daddi, Guglielmo and Royce, Rob and Tosi, Phillipe and Gildner, Matthew and Vaquero, Tiago and Veismann, Marcel and others},
-            booktitle={2023 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)},
-            pages={9886--9893},
-            year={2023},
-            organization={IEEE}
+    title={EELS: Towards Autonomous Mobility in Extreme Terrain with a Versatile Snake Robot with Resilience to Exteroception Failures},
+    author={Thakker, Rohan and Paton, Michael and Strub, Marlin P and Swan, Michael and Daddi, Guglielmo and Royce, Rob and Tosi, Phillipe and Gildner, Matthew and Vaquero, Tiago and Veismann, Marcel and others},
+    booktitle={2023 IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)},
+    pages={9886--9893},
+    year={2023},
+    organization={IEEE}
+    }`
+    },
+    {
+        id: `daddi2022thermal`,
+        title: 'Thermal probe enhanced with pulsed plasma discharges for efficient ice penetration',
+        bibtex: `@article{daddi2022thermal,
+title={Thermal probe enhanced with pulsed plasma discharges for efficient ice penetration},
+author={Daddi, Guglielmo and Mier-Hicks, Fernando},
+journal={Journal of Thermophysics and Heat Transfer},
+volume={36},
+number={1},
+pages={81--88},
+year={2022},
+publisher={American Institute of Aeronautics and Astronautics}
+}`
+    },
+    {
+        id: `ono2024boldly`,
+        title: `To Boldly Go Where No Robots Have Gone Before--Part 1: EELS Robot to Spearhead a New One-Shot Exploration Paradigm with in-situ Adaptation`,
+        bibtex: `@inproceedings{ono2024boldly,
+title={To Boldly Go Where No Robots Have Gone Before--Part 1: EELS Robot to Spearhead a New One-Shot Exploration Paradigm with in-situ Adaptation},
+author={Ono, Masahiro and Thakker, Rohan and Georgiev, Nikola and Gavrilov, Peter and Archanian, Avak and Drevinskas, Tomas and Daddi, Guglielmo and Paton, Michael and Melikyan, Hovhannes and Pailevanian, Torkom and others},
+booktitle={AIAA Scitech 2024 Forum},
+pages={1746},
+year={2024}
+}`
+    },
+    {
+        id: `thakker2024boldly`,
+        title: `To Boldly Go Where No Robots Have Gone Before--Part 4: NEO Autonomy for Robustly Exploring Unknown, Extreme Environments with Versatile Robots`,
+        bibtex: `@inproceedings{thakker2024boldly,
+title={To Boldly Go Where No Robots Have Gone Before--Part 4: NEO Autonomy for Robustly Exploring Unknown, Extreme Environments with Versatile Robots},
+author={Thakker, Rohan and Paton, Michael and Strub, Marlin and Swan, Michael and Vaquero, Tiago and Jones, Bryson and Daddi, Guglielmo and Royce, Rob and Bowkett, Joseph and Loret de Mola Lemus, Daniel and others},
+booktitle={AIAA SCITECH 2024 Forum},
+pages={1747},
+year={2024}
           }`
-    }
+    },
+    {
+        id: `ono2023enabling`,
+        title: `Enabling Ocean Access with EELS`,
+        bibtex: `@article{ono2023enabling,
+title={Enabling Ocean Access with EELS},
+author={Ono, Masahiro and Cable, Morgan L and Spilker, Linda Joyce and Drevinskas, Tomas and Weber, Jessica and Malaska, Michael and Gardner, Alex S and Ambrose, Eric and Archanian, Avak and Barchowsky, Ansel and others},
+journal={AGU23},
+year={2023},
+publisher={AGU}
+}`
+    },
+    {
+        id: `daddi2022assessing`,
+        title: `Assessing an Image-to-Image Approach to Global Path Planning for a Planetary Exploration`,
+        bibtex: `@article{daddi2022assessing,
+title={Assessing an Image-to-Image Approach to Global Path Planning for a Planetary Exploration},
+author={Daddi, Guglielmo and Notaristefano, Nicolaus and Stesina, Fabrizio and Corpino, Sabrina},
+journal={Aerospace},
+volume={9},
+number={11},
+pages={721},
+year={2022},
+publisher={MDPI}
+}`
+    },
+    {
+        id: `corpino2022space`,
+        title: 'Space Rider Observer Cube--SROC: a CubeSat mission for proximity operations demonstration',
+        bibtex: `@inproceedings{corpino2022space,
+  title={Space Rider Observer Cube--SROC: a CubeSat mission for proximity operations demonstration},
+  author={Corpino, S and Ammirante, G and Daddi, G and Stesina, F and Corradino, F and Basler, A and Francesconi, A and Branz, F and Van den Eynde, J and others},
+  booktitle={Proc. 73rd International Astronautical Congress (IAC)},
+  year={2022}
+  }`
+      } 
 ];
